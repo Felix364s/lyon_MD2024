c37ae8e431ba26a0519064704432c7b820bb2f67
Felix364s <felixvalenzuela789@gmail.com>
2026-07-02 14:26:37 -0600

    Renombrar a app.css y actualizar import en main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 import App from "./App"
 import "./index.css"
-import "./App.css"
+import "./app.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
