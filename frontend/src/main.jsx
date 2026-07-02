b4b31e155ebc99e3017b4d7cbf59a766d61e51f7
Felix364s <felixvalenzuela789@gmail.com>
2026-07-02 14:21:42 -0600

    Corregir mayúscula en import de App.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 import App from "./App"
 import "./index.css"
-import "./app.css"
+import "./App.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
